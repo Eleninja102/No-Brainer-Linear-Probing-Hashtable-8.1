42ce6c264440e29d1f1d5a4c2db3f5204465bd39
Coleton Watt <electricninja102@gmail.com>
2022-03-07 22:36:19 -0700

    Initial Commit

---
+++ No Brainer: Linear Probing Hashtable 8.1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  No Brainer: Linear Probing Hashtable 8.1
+//
+//  Created by Coleton Watt on 3/7/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
